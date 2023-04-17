95581ff19b7e810fc9f9fedc5994f8a1851290ba
Andrew <andrewluferenko@icloud.com>
2023-04-17 23:00:54 +0300

    for check

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,48 +4,81 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    int resumesCount = 10000;
+    Resume[] storage = new Resume[resumesCount];
     int size;
 
     void clear() {
+        int count = 0;
         for (int i = 0; i < size; i++) {
-            storage[i] = null;
+            if (storage[i] != null) {
+                count++;
+            }
         }
+
+        Arrays.fill(storage, 0, count, null);
         size = 0;
     }
 
     void save(Resume r) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(r.uuid)) {
+                System.out.println("Error: резюме с uuid " + r.uuid + " уже существует");
+                return;
+            }
+        }
+
+        if (size == resumesCount) {
+            System.out.println("Error: переполнение списка");
+        }
+
         storage[size++] = r;
     }
 
+    void update(Resume resume) {
+        if (checkResumes(resume.uuid)) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equals(resume.uuid)) {
+                    storage[i] = resume;
+                } else {
+                    return;
+                }
+            }
+        }
+    }
+
     Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                return storage[i];
+        if (checkResumes(uuid)) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    return storage[i];
+                }
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        int index = -1;
+        if (checkResumes(uuid)) {
+            int index = -1;
 
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                index = i;
+            for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    index = i;
+                }
             }
-        }
 
-        if (index == -1) {
-            return;
-        }
+            if (index == -1) {
+                return;
+            }
 
-        while (index < size - 1) {
-            storage[index] = storage[index + 1];
-            index++;
+            while (index < size - 1) {
+                storage[index] = storage[index + 1];
+                index++;
+            }
+            storage[index] = null;
+            size--;
         }
-        storage[index] = null;
-        size--;
     }
 
     /**
@@ -58,4 +91,19 @@ public class ArrayStorage {
     int size() {
         return size;
     }
+
+    boolean checkResumes(String uuid) {
+        boolean checkResume = false;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                checkResume = true;
+                break;
+            }
+        }
+        if (checkResume == false) {
+            System.out.println("Error: резюме c uuid " + uuid + " не существует");
+        }
+        return checkResume;
+    }
 }
+
