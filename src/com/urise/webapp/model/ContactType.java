1ba1bc526cfabf41bb8ac3d3f618331ccfe43435
Andrew <andrewluferenko@icloud.com>
2023-06-19 16:09:12 +0300

    lesson 7 (finished) 19.06.23

--- src/com/urise/webapp/model/ContactType.java
+++ src/com/urise/webapp/model/ContactType.java
@@ -9,7 +9,7 @@ public enum ContactType {
     PROFILESTACKOVERFLOW("Профиль StackOverflow"),
     HOMEPAGE("Домшнаяя страница");
 
-    private String title;
+    private final String title;
 
     ContactType(String title) {
         this.title = title;
