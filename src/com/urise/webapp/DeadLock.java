84a47c42c95f4fc6912fcfc42a80efd504d97de4
Andrew <andfrewluferenko@icloud.com>
2023-08-15 20:04:20 +0300

    lesson 11 (finished after check 15.08.2023)

--- src/com/urise/webapp/DeadLock.java
+++ src/com/urise/webapp/DeadLock.java
@@ -3,35 +3,28 @@ package com.urise.webapp;
 public class DeadLock {
     private static final Object lock1 = new Object();
     private static final Object lock2 = new Object();
+
     public static void main(String[] args) {
-        deadLock();
+        Thread thread1 = new Thread(deadLock(lock1, lock2));
+        Thread thread2 = new Thread(deadLock(lock2, lock1));
+        thread1.start();
+        thread2.start();
     }
 
-    private static void deadLock() {
-        Thread thread1 = new Thread(() -> {
+    private static Thread deadLock(Object lock1, Object lock2) {
+        return new Thread(() -> {
             System.out.println(Thread.currentThread().getName() + " start");
             synchronized (lock1) {
                 try {
-                    Thread.sleep(3000);
+                    Thread.sleep(100);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
                 synchronized (lock2) {
                 }
+                System.out.println(Thread.currentThread().getName() + " end");
             }
             System.out.println(Thread.currentThread().getName() + " end");
-        }, "Thread1");
-
-        Thread thread2 = new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + " start");
-            synchronized (lock2) {
-                synchronized (lock1) {
-                }
-            }
-            System.out.println(Thread.currentThread().getName() + " end");
-        }, "Thread2");
-
-        thread1.start();
-        thread2.start();
+        });
     }
 }
